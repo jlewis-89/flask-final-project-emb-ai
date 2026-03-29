4cba4c6d5d5f102d68f8819b073351be29f6a1af
jlewis-89 <jlewis21689@live.com>
2026-03-29 11:13:03 -0400

    Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,23 +2,12 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-
+    
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
     input_json = {"raw_document": {"text": text_to_analyze}}
-
-    # Test formatting for error handling
-    if not text_to_analyze:
-        return response.status_code == 400, {
-        "anger": None,
-        "disgust": None,
-        "fear": None,
-        "joy": None,
-        "sadness": None,
-        "dominant_emotion": None
-        }
 
     try:
         response = requests.post(url, headers=headers, json=input_json, timeout=10)
@@ -26,6 +15,7 @@ def emotion_detector(text_to_analyze):
         emotions = data["emotionPredictions"][0]["emotion"]
         dominant_emotion = max(emotions, key=emotions.get)
         return {
+        "response": response.status_code,
         "anger": emotions["anger"],
         "disgust": emotions["disgust"],
         "fear": emotions["fear"],
@@ -33,7 +23,16 @@ def emotion_detector(text_to_analyze):
         "sadness": emotions["sadness"],
         "dominant_emotion": dominant_emotion
         }
-
-    except Exception as e:
-        print("Request failed:", e)
-        return None
+    
+    except KeyError:
+        if response.status_code == 400:
+            return {
+                "response": response.status_code,
+                "anger": None,
+                "disgust": None,
+                "fear": None,
+                "joy": None,
+                "sadness": None,
+                "dominant_emotion": None
+                }
+    
--- server.py
+++ server.py
@@ -11,7 +11,18 @@ def index():
 def emotiondetector():
     text = request.args.get("textToAnalyze")
     result = emotion_detector(text)
-    return (f"For the given statement, the system response is {result}")
-
+    response = result['response']
+    anger = result['anger']
+    disgust = result['disgust']
+    fear = result['fear']
+    joy = result['joy']
+    sadness = result['sadness']
+    dominant_emotion = result['dominant_emotion']
+    if response == 400:
+        return  "Invalid text! Please try again!."
+    else:
+        return (f"For the given statement, the system response is \'anger\': {anger}, \'disgust\': {disgust}, \'fear\': {fear}, \'joy\': {joy} and \'sadness\': {sadness}. The dominant emotion is {dominant_emotion}.")
+    
+    
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
